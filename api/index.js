7ae24d05f0d12c9a625fc81566fb74954b43718c
jeet250904 <22it011@charusat.edu.in>
2024-10-08 22:36:03 +0530

    changed

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(5000, () => {
+app.listen(3000, () => {
   console.log('Server is running on port 3000!');
 });
 
